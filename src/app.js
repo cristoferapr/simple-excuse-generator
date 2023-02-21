17088a674a70d860ca880a44355bdec3b07c05a2
cristofer-apr <cpr.refor@gmail.com>
2023-02-21 07:54:38 +0000

    implementacion

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const randomItem = array => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const generate = (a, b, c, d) => {
+  return (
+    randomItem(a) +
+    " " +
+    randomItem(b) +
+    " " +
+    randomItem(c) +
+    " " +
+    randomItem(d)
+  );
+};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse = generate(who, action, what, when);
+  function changeText() {
+    var element = document.getElementById("excuse");
+    element.innerHTML = excuse;
+  }
+  changeText();
 };
